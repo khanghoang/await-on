83479c18369e9ce05aafc2b7460c1b68295a46b9
Khang Hoang <hoangtrieukhang@gmail.com>
2018-05-31 17:27:42 -0700

    fix tests

--- src/await-on.js
+++ src/await-on.js
@@ -1,6 +1,6 @@
 /* @flow */
 
-export default <R, E>(promise: Promise<R>): Promise<[R | null, E | void]> =>
+module.exports = <R, E>(promise: Promise<R>): Promise<[R | null, E | void]> =>
   new Promise(resolve =>
     promise.then(
       (result: R) => resolve([result, undefined]),
--- src/await-on.test.js
+++ src/await-on.test.js
@@ -1,19 +1,19 @@
 /* @flow */
-import to from './try';
+const on = require('./await-on');
 
 describe('Try ', () => {
   it('return result', async () => {
-    const [result, error] = await to(Promise.resolve(1));
+    const [result, error] = await on(Promise.resolve(1));
     expect(result).toEqual(1);
     expect(error).toBeUndefined();
   });
   it('return the final result', async () => {
-    const [result, error] = await to(Promise.resolve(1).then(() => 2));
+    const [result, error] = await on(Promise.resolve(1).then(() => 2));
     expect(result).toEqual(2);
     expect(error).toBeUndefined();
   });
   it('returns error', async () => {
-    const [result, error] = await to(Promise.reject(new Error()));
+    const [result, error] = await on(Promise.reject(new Error()));
     expect(result).toBeNull();
     expect(error).toBeInstanceOf(Error);
   });
